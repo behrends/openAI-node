690ad26bb5682e47a58b6f759601152d7ba34fff
Erik Behrends <behrends@users.noreply.github.com>
2024-03-25 15:09:13 +0100

    Read prompt from command line

--- main.js
+++ main.js
@@ -5,13 +5,26 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+let input = null;
+// check if prompt given on command line
+if (process.argv.length > 2) {
+  input = process.argv[2];
+} else {
+  // no prompt given
+  console.error(
+    `Dein Prompt fehlt. Gib ihn so im Programmaufruf an:
+        node main.js 'Mein Prompt ist hier.' `
+  );
+  process.exit();
+}
+
 const chatCompletion = await openai.chat.completions.create({
   messages: [
     {
       role: 'system',
       content: `Du bist ein Experte in JavaScript, der dabei hilft, die Sprache zu erlernen.`,
       role: 'user',
-      content: `Erkläre mir Arrays.`,
+      content: input, // prompt given on command line
     },
   ],
   model: 'gpt-4-turbo-preview',
