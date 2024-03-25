8d2c5ec7b263560635664b1a4006ee4ba8a45479
Erik Behrends <behrends@users.noreply.github.com>
2024-03-25 15:02:28 +0100

    Use GPT4

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const chatCompletion = await openai.chat.completions.create({
       content: `Erkläre mir Arrays.`,
     },
   ],
-  model: 'gpt-3.5-turbo',
+  model: 'gpt-4-turbo-preview',
 });
 
 console.log(chatCompletion.choices[0].message.content);
